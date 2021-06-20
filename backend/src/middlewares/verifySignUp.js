ae1e348272bb9f374356c2408a2949c6333b1c22
alonso804 <alonsobs804@gmail.com>
2021-06-19 21:39:49 -0500

    update backend for sonar

--- backend/src/middlewares/verifySignUp.js
+++ backend/src/middlewares/verifySignUp.js
@@ -1,7 +1,9 @@
 import User from "../models/User";
 
 export const checkDuplicateUsername = async (req, res, next) => {
-  const username = await User.findOne({ username: req.body.username });
+  const username = await User.findOne({
+    username: req.body.username.toString(),
+  });
 
   if (username) {
     return res.status(400).json({ ok: false, message: "El usuario ya existe" });
@@ -11,7 +13,7 @@ export const checkDuplicateUsername = async (req, res, next) => {
 };
 
 export const checkDuplicateEmail = async (req, res, next) => {
-  const email = await User.findOne({ email: req.body.email });
+  const email = await User.findOne({ email: req.body.email.toString() });
 
   if (email) {
     return res
